b5e6eaa7437b892a67863616c8f6e8f91a5363a0
AvocadoWitchKnits <knittingtogethersa@gmail.com>
2026-08-23 12:32:34 +0200

    Cleaned up inventory system

--- Shoot for the Stars/Assets/Scripts/Anne Scripts (Inventory, Item Spawn, Crafting)/InventoryUI.cs
+++ Shoot for the Stars/Assets/Scripts/Anne Scripts (Inventory, Item Spawn, Crafting)/InventoryUI.cs
@@ -10,8 +10,6 @@ public class InventoryUI : MonoBehaviour
 
     [Header("References")]
     [SerializeField]
-    Inventory inventory;
-    [SerializeField]
     Transform uiInventoryParent;
 
     [Header("State")]
@@ -20,37 +18,10 @@ public class InventoryUI : MonoBehaviour
 
     public void AddUIItem(string inventoryId, Item item)
     {
-        Debug.Log("AddUIItem started");
-
         var itemUI = Instantiate(uiItemPrefab, uiInventoryParent).GetComponent<ItemUI>();
-
-        Debug.Log("UI prefab instantiated");
-        Debug.Log("itemUI is null: " + (itemUI == null));
-        Debug.Log("item is null before Initialize: " + (item == null));
-        Debug.Log("inventory is null: " + (inventory == null));
-        Debug.Log("uiInventoryParent is null: " + (uiInventoryParent == null));
 
         inventoryUI.Add(inventoryId, itemUI.gameObject);
 
-        Debug.Log("About to call Initialize");
-
-        itemUI.Initialize(inventoryId, item, inventory.DropItem);
-
-        Debug.Log("Initialize completed");
-    }
-
-    // public void AddUIItem(string inventoryId, Item item)
-    // {
-    //  var itemUI = Instantiate(uiItemPrefab, uiInventoryParent).GetComponent<ItemUI>();
-
-    //        inventoryUI.Add(inventoryId, itemUI.gameObject);
-    //        itemUI.Initialize(inventoryId, item, inventory.DropItem);
-    //   }
-
-    public void RemoveUIItem(string inventoryId)
-    {
-        var itemUI = inventoryUI.GetValueOrDefault(inventoryId);
-        inventoryUI.Remove(inventoryId);
-        Destroy(itemUI);
+        itemUI.Initialize(item);
     }
 }
--- Shoot for the Stars/Assets/Scripts/Anne Scripts (Inventory, Item Spawn, Crafting)/ItemUI.cs
+++ Shoot for the Stars/Assets/Scripts/Anne Scripts (Inventory, Item Spawn, Crafting)/ItemUI.cs
@@ -2,27 +2,16 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Button))]
 public class ItemUI : MonoBehaviour
 {
-    [Header("References")]
     [SerializeField]
     Image image;
-    [SerializeField]
-    Button button;
 
-    public void Initialize(string inventoryId, Item item, Action<string> removeItemAction)
+    public void Initialize(Item item)
     {
-        Debug.Log("Image is null: " + (image == null));
-        Debug.Log("Item is null: " + (item == null));
-
         image.sprite = item.icon;
         transform.localScale = Vector3.one;
-        button.onClick.AddListener(() => removeItemAction.Invoke(inventoryId));
     }
 
-    void OnDestroy()
-    {
-        button.onClick.RemoveAllListeners();
-    }
+
 }
