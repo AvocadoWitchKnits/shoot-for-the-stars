69578af46c7178fb3089bed60e25bf18361b400e
AvocadoWitchKnits <knittingtogethersa@gmail.com>
2026-08-24 11:11:04 +0200

    Fixing merge errors

--- Shoot for the Stars/Assets/Scripts/Anne Scripts (Inventory, Item Spawn, Crafting)/Spawner.cs
+++ Shoot for the Stars/Assets/Scripts/Anne Scripts (Inventory, Item Spawn, Crafting)/Spawner.cs
@@ -1,16 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Spawner : MonoBehaviour
-{
-    public GameObject cubePrefab;
-
-    private void Start()
-    {
-        Instantiate(cubePrefab, transform.position, Quaternion.identity);
-    }
-}
 
 
 
