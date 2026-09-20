fd1366b026ff58cc710c66dfa31801a35af3b6ba
AvocadoWitchKnits <knittingtogethersa@gmail.com>
2026-09-20 11:33:44 +0200

    Inventory Overhaul continued

--- Shoot for the Stars/Assets/Scripts/Anne Scripts (Inventory, Item Spawn, Crafting)/Item.cs
+++ Shoot for the Stars/Assets/Scripts/Anne Scripts (Inventory, Item Spawn, Crafting)/Item.cs
@@ -1,11 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-[CreateAssetMenu(fileName = "Item", menuName = "RumpledCode/Item", order = 1)]
+
+[CreateAssetMenu(menuName = "Scriptable object/Item")]
 public class Item : ScriptableObject
 
 {
+    public Item item;
+    public Image image;
+    private void Start()
+    {
+        InitialiseItem(item);
+    }
+    public void InitialiseItem(Item newItem)
+    {
+        image.sprite = newItem.image;
+    }
     public string id;
     public string description;
     public Sprite icon;
     public GameObject prefab;
+    public bool stackable = true;
+
+
 }
