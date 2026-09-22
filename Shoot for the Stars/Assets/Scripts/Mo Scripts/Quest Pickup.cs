cdb33cdc07f3cb0cda64b6f30f8f2882c73d1246
Mosimaart <bmc.mamahlodi@gmail.com>
2026-09-22 23:37:17 +0200

    Miscell

--- Shoot for the Stars/Assets/Scripts/Mo Scripts/Quest Pickup.cs
+++ Shoot for the Stars/Assets/Scripts/Mo Scripts/Quest Pickup.cs
@@ -3,33 +3,34 @@ using System.Collections;
 public class QuestPickupItem : MonoBehaviour
 {
     public string itemName;
-    
+
 
     private bool isBeingCollected = false;
-public void SuckIn(Transform target, float speed)
-{
-    if (isBeingCollected) return;
-    isBeingCollected = true;
-
-    Debug.Log("SuckIn started for: " + itemName);
-
-    Collider col = GetComponent<Collider>();
-    if (col != null) col.enabled = false;
-
-    
-
-    StartCoroutine(SuckInRoutine(target, speed));
-}
-
-private IEnumerator SuckInRoutine(Transform target, float speed)
-{
-    while (target != null && Vector3.Distance(transform.position, target.position) > 0.2f)
+    public void SuckIn(Transform target, float speed)
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        yield return null;
+        if (isBeingCollected) return;
+        isBeingCollected = true;
+
+        Debug.Log("SuckIn started for: " + itemName);
+
+        Collider col = GetComponent<Collider>();
+        if (col != null) col.enabled = false;
+
+
+
+        StartCoroutine(SuckInRoutine(target, speed));
     }
 
-    Debug.Log("Item collected, calling QuestManager for: " + itemName);
-    QuestManager.Instance.OnItemPickedUp(itemName);
-    Destroy(gameObject);
-}}
+    private IEnumerator SuckInRoutine(Transform target, float speed)
+    {
+        while (target != null && Vector3.Distance(transform.position, target.position) > 0.2f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        Debug.Log("Item collected, calling QuestManager for: " + itemName);
+        QuestManager.Instance.OnItemPickedUp(itemName);
+        Destroy(gameObject);
+    }
+}
